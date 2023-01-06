24349045ed7e102c99b9a7cc51f4718522af2385
Alberto <94637799+meaar@users.noreply.github.com>
2023-01-06 13:09:33 -0300

    added none user condition

--- cluster-install.sh
+++ cluster-install.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-user=$USER
+user=
 address=
 while [[ -z $address ]]; do
   read -p "SSH Address of host [none]: " address
 done
-read -p "SSH User of host ($address) [$user]: " user
+read -p "SSH User of host ($address) [$USER]: " user
+if [[ -z $user ]]; then
+  user=$USER
+fi
 sudo apt update
 sudo apt install -y ca-certificates curl gnupg lsb-release apt-transport-https
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo service docker start
-sudo apt install openssh-client openssh-server -y
+sudo apt install -y openssh-client openssh-server
 ssh-keygen
 sudo service ssh start
 ssh-copy-id -i ~/.ssh/id_rsa.pub $user@$address
@@ -33,5 +36,5 @@ tar -zxvf k9s_Linux_x86_64.tar.gz
 sudo chmod +x k9s
 sudo mv k9s /bin/
 mkdir ~/.kube
-mv ~/kube_config_cluster.yml ~/.kube/config
+cp kube_config_cluster.yml ~/.kube/config
 k9s
