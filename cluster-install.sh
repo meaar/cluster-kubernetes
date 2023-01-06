a8113789d9d91edfd1793f9a4d3a07b8d9eb0477
Alberto <94637799+meaar@users.noreply.github.com>
2023-01-05 23:35:26 -0300

    added k9s and improved dependency formatting

--- cluster-install.sh
+++ cluster-install.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 sudo apt update
-sudo apt install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+sudo apt install -y ca-certificates curl gnupg lsb-release apt-transport-https
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo service docker start
 sudo apt install openssh-client openssh-server -y
 sudo ssh-keygen
 sudo service ssh start
-###ssh-copy-id -i ~/.ssh/id_rsa.pub YOUR_USER_NAME@IP_ADDRESS_OF_THE_SERVER nas maquinas
-###rke download https://github.com/rancher/rke/releases/download/v1.4.1/rke_linux-amd64
-###rke config
-###rke up
-sudo apt install -y ca-certificates curl
-sudo apt install -y apt-transport-https
+# ssh-copy-id -i ~/.ssh/id_rsa.pub YOUR_USER_NAME@IP_ADDRESS_OF_THE_SERVER
+wget https://github.com/rancher/rke/releases/download/v1.4.1/rke_linux-amd64
+sudo chmod +x rke_linux-amd64
+sudo rke_linux-amd64 /bin/rke
+rke config
+# rke up
 sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
 sudo apt install -y kubectl
-###k9s download https://github.com/derailed/k9s/releases/download/v0.26.7/k9s_Linux_x86_64.tar.gz
+wget https://github.com/derailed/k9s/releases/download/v0.26.7/k9s_Linux_x86_64.tar.gz
+tar -xf k9s_Linux_x86_64.tar.gz
+sudo chmod +x k9s
+sudo mv k9s /bin/
+mkdir ~/.kube
